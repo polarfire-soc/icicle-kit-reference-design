29e5a80e62dca470a30fd908212f104ebc1dc2ff
hughbreslin <hughbreslin@users.noreply.github.com>
2020-12-04 08:31:43 +0000

    Fixed the component folder generation by importing instead of linking the SDIO register. Fixed a complie error caused by a missing ' " ' in the TCL parameters file

--- script_support/hdl_source.tcl
+++ script_support/hdl_source.tcl
@@ -1,10 +1,10 @@
 #Importing and Linking all the HDL source files used in the design
-create_links \
+import_files \
          -convert_EDN_to_HDL 0 \
          -library {work} \
          -hdl_source {script_support/HDL/SDIO_register.v} 
 build_design_hierarchy 
-create_hdl_core -file "$local_dir/script_support/HDL/SDIO_register.v" -module {sdio_register} -library {work} -package {} 
+create_hdl_core -file $project_dir/hdl/SDIO_register.v -module {sdio_register} -library {work} -package {} 
 hdl_core_add_bif -hdl_core_name {sdio_register} -bif_definition {APB:AMBA:AMBA2:slave} -bif_name {APBslave} -signal_map {} 
 hdl_core_assign_bif_signal -hdl_core_name {sdio_register} -bif_name {APBslave} -bif_signal_name {PADDR} -core_signal_name {paddr} 
 hdl_core_assign_bif_signal -hdl_core_name {sdio_register} -bif_name {APBslave} -bif_signal_name {PENABLE} -core_signal_name {penable} 

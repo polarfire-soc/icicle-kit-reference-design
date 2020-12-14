42571171b3ff8992fc1138de532bce78c16348a2
hughbreslin <hughbreslin@users.noreply.github.com>
2020-12-14 11:29:03 +0000

    Added download command for PF SRAM as the scripts will fail if this core is not present in a vault

--- ICICLE_KIT_SD_CARD.tcl
+++ ICICLE_KIT_SD_CARD.tcl
@@ -50,6 +50,7 @@ download_core -vlnv {Actel:SgCore:PF_XCVR_REF_CLK:1.0.103} -location {www.microc
 download_core -vlnv {Actel:DirectCore:CoreAPB3:4.1.100} -location {www.microchip-ip.com/repositories/DirectCore}
 download_core -vlnv {Actel:DirectCore:COREAXI4DMACONTROLLER:2.0.100} -location {www.microchip-ip.com/repositories/DirectCore}
 download_core -vlnv {Actel:DirectCore:CoreGPIO:3.2.102} -location {www.microchip-ip.com/repositories/DirectCore}
+download_core -vlnv {Actel:SystemBuilder:PF_SRAM_AHBL_AXI:1.2.108} -location {www.microchip-ip.com/repositories/SgCore}
 
 source ./script_support/MPFS_ICICLE_SD_CARD_recursive.tcl
 

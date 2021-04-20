3390afed848fc26affacb58d0549078753287ab8
hughbreslin <hughbreslin@users.noreply.github.com>
2021-04-20 20:08:10 +0100

    Removed commands to build the design hierarchy in the recursive scripts and relocated where the hdl source scipt is being called, this reduces overall run time significantly.
    Tested the scripts are executing successfully for both eMMC and SD Card.

--- script_support/MPFS_ICICLE_eMMC_recursive.tcl
+++ script_support/MPFS_ICICLE_eMMC_recursive.tcl
@@ -1,15 +1,10 @@
-#This Tcl file sources other Tcl files to build the design(on which recursive export is run) in a bottom-up fashion
-
-#Sourcing the Tcl file in which all the HDL source files used in the design are imported or linked
-build_design_hierarchy
-
-#Sourcing the Tcl files for creating individual components under the top level
 if {[file isdirectory $local_dir/script_support/components/MSS_eMMC]} {
 	file delete -force $local_dir/script_support/components/MSS_eMMC
 }
 file mkdir $local_dir/script_support/components/MSS_eMMC
 exec $mss_config_loc -CONFIGURATION_FILE:$local_dir/script_support/PF_SoC_MSS_Icicle_eMMC.cfg -OUTPUT_DIR:$local_dir/script_support/components/MSS_eMMC
 import_mss_component -file "$local_dir/script_support/components/MSS_eMMC/ICICLE_MSS.cxz"
+source script_support/hdl_source.tcl
 source script_support/components/CORERESET_0.tcl
 source script_support/components/INIT_MONITOR.tcl 
 source script_support/components/PCIE_MASTER.tcl 
@@ -28,11 +23,7 @@ source script_support/components/DMA_MASTER.tcl
 source script_support/components/GPIO.tcl
 source script_support/components/FIC3_MASTER.tcl
 source script_support/components/OSCILLATOR_160MHz.tcl
-source script_support/hdl_source.tcl
 source script_support/components/CLOCKS_AND_RESETS.tcl 
 source script_support/components/MPFS_ICICLE_eMMC.tcl 
-build_design_hierarchy
 set_root -module {MPFS_ICICLE_KIT_BASE_DESIGN::work} 
-build_design_hierarchy 
 source script_support/simulation/Test_bench.tcl
-build_design_hierarchy

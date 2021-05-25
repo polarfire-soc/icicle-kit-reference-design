d1bb90c702fe4b5181849f007aa90082fda5f159
hughbreslin <hughbreslin@users.noreply.github.com>
2021-05-25 17:20:28 +0100

    Removed test bench generation by default from the SD card recursive script - this should have been removed previously as its now part of arguments in the top level script

--- script_support/MPFS_ICICLE_SD_CARD_recursive.tcl
+++ script_support/MPFS_ICICLE_SD_CARD_recursive.tcl
@@ -26,4 +26,3 @@ source script_support/components/OSCILLATOR_160MHz.tcl
 source script_support/components/CLOCKS_AND_RESETS.tcl 
 source script_support/components/MPFS_ICICLE_SD_CARD.tcl 
 set_root -module {MPFS_ICICLE_KIT_BASE_DESIGN::work} 
-source script_support/simulation/Test_bench.tcl

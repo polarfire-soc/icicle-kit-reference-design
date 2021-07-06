1db7dbbd09bd23f71e67426da459d218fa6ffd97
Hugh Breslin <Hugh.Breslin@microchip.com>
2021-07-06 09:59:47 +0100

    Add corePWM TCL description file and generate the core as part of the recursive scripts

--- script_support/MPFS_ICICLE_eMMC_recursive.tcl
+++ script_support/MPFS_ICICLE_eMMC_recursive.tcl
@@ -23,6 +23,7 @@ source script_support/components/DMA_MASTER.tcl
 source script_support/components/GPIO.tcl
 source script_support/components/FIC3_MASTER.tcl
 source script_support/components/OSCILLATOR_160MHz.tcl
+source script_support/components/COREPWM_C0.tcl
 source script_support/components/CLOCKS_AND_RESETS.tcl 
 source script_support/components/MPFS_ICICLE_eMMC.tcl 
 set_root -module {MPFS_ICICLE_KIT_BASE_DESIGN::work} 

4d50bb0ef72b039f5a114185346381b2dbd78a83
Hugh Breslin <Hugh.Breslin@microchip.com>
2021-08-16 17:44:38 +0100

    Create HDL+ core from IPC_mailbox Fix missing backslash in -hdl_source command

--- script_support/hdl_source.tcl
+++ script_support/hdl_source.tcl
@@ -2,7 +2,7 @@
 import_files \
          -convert_EDN_to_HDL 0 \
          -library {work} \
-         -hdl_source {script_support/HDL/SDIO_register.v} 
+         -hdl_source {script_support/HDL/SDIO_register.v} \
          -hdl_source {script_support/HDL/apb_arbiter.v} \
          -hdl_source {script_support/HDL/ipc_mailbox.v} \
          -hdl_source {script_support/HDL/mailbox_ctrl.v} \
@@ -51,3 +51,24 @@ hdl_core_assign_bif_signal -hdl_core_name {APB_ARBITER} -bif_name {APB_MASTER_0x
 hdl_core_assign_bif_signal -hdl_core_name {APB_ARBITER} -bif_name {APB_MASTER_0x5xxx_xxxx} -bif_signal_name {PREADY} -core_signal_name {out_0x5xxx_xxxx_pready} 
 hdl_core_assign_bif_signal -hdl_core_name {APB_ARBITER} -bif_name {APB_MASTER_0x5xxx_xxxx} -bif_signal_name {PSLVERR} -core_signal_name {out_0x5xxx_xxxx_pslverr} 
 hdl_core_assign_bif_signal -hdl_core_name {APB_ARBITER} -bif_name {APB_MASTER_0x5xxx_xxxx} -bif_signal_name {PSELx} -core_signal_name {out_0x5xxx_xxxx_psel} 
+
+create_hdl_core -file $project_dir/hdl/ipc_mailbox.v -module {IPC_MAILBOX} -library {work} -package {} 
+hdl_core_add_bif -hdl_core_name {IPC_MAILBOX} -bif_definition {APB:AMBA:AMBA2:mirroredMaster} -bif_name {A_SIDE} -signal_map {} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PADDR} -core_signal_name {a_paddr} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PENABLE} -core_signal_name {a_penable} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PWRITE} -core_signal_name {a_pwrite} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PRDATA} -core_signal_name {a_prdata} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PWDATA} -core_signal_name {a_pwdata} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PREADY} -core_signal_name {a_pready} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PSLVERR} -core_signal_name {a_pslverr} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PSELx} -core_signal_name {a_psel} 
+hdl_core_add_bif -hdl_core_name {IPC_MAILBOX} -bif_definition {APB:AMBA:AMBA2:mirroredMaster} -bif_name {B_SIDE} -signal_map {} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PADDR} -core_signal_name {b_paddr} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PENABLE} -core_signal_name {b_penable} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PWRITE} -core_signal_name {b_pwrite} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PRDATA} -core_signal_name {b_prdata} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PWDATA} -core_signal_name {b_pwdata} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PREADY} -core_signal_name {b_pready} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PSLVERR} -core_signal_name {b_pslverr} 
+hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PSELx} -core_signal_name {b_psel} 
+

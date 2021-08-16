3f1ab60256c982fea32883575c7388a8863930a6
Hugh Breslin <Hugh.Breslin@microchip.com>
2021-08-16 17:55:42 +0100

    Update interface type from mirroredmaster to slave on IPC_MAILBOX

--- script_support/hdl_source.tcl
+++ script_support/hdl_source.tcl
@@ -53,7 +53,7 @@ hdl_core_assign_bif_signal -hdl_core_name {APB_ARBITER} -bif_name {APB_MASTER_0x
 hdl_core_assign_bif_signal -hdl_core_name {APB_ARBITER} -bif_name {APB_MASTER_0x5xxx_xxxx} -bif_signal_name {PSELx} -core_signal_name {out_0x5xxx_xxxx_psel} 
 
 create_hdl_core -file $project_dir/hdl/ipc_mailbox.v -module {IPC_MAILBOX} -library {work} -package {} 
-hdl_core_add_bif -hdl_core_name {IPC_MAILBOX} -bif_definition {APB:AMBA:AMBA2:mirroredMaster} -bif_name {A_SIDE} -signal_map {} 
+hdl_core_add_bif -hdl_core_name {IPC_MAILBOX} -bif_definition {APB:AMBA:AMBA2:slave} -bif_name {A_SIDE} -signal_map {} 
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PADDR} -core_signal_name {a_paddr} 
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PENABLE} -core_signal_name {a_penable} 
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PWRITE} -core_signal_name {a_pwrite} 
@@ -62,7 +62,7 @@ hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PREADY} -core_signal_name {a_pready} 
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PSLVERR} -core_signal_name {a_pslverr} 
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {A_SIDE} -bif_signal_name {PSELx} -core_signal_name {a_psel} 
-hdl_core_add_bif -hdl_core_name {IPC_MAILBOX} -bif_definition {APB:AMBA:AMBA2:mirroredMaster} -bif_name {B_SIDE} -signal_map {} 
+hdl_core_add_bif -hdl_core_name {IPC_MAILBOX} -bif_definition {APB:AMBA:AMBA2:slave} -bif_name {B_SIDE} -signal_map {} 
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PADDR} -core_signal_name {b_paddr} 
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PENABLE} -core_signal_name {b_penable} 
 hdl_core_assign_bif_signal -hdl_core_name {IPC_MAILBOX} -bif_name {B_SIDE} -bif_signal_name {PWRITE} -core_signal_name {b_pwrite} 

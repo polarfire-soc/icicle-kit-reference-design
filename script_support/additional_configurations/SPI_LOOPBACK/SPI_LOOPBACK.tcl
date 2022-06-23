156e806557e29642abb686859f684a2111a88fa5
Hugh Breslin <Hugh.Breslin@microchip.com>
2022-06-23 12:31:41 +0100

    Update SPI_LOOPBACK argument design Tcl
    Updated the SPI loopback design Tcl to take into account design changes.
    This Tcl now disconnected QSPI from the RPI interface and connectes SPI0
    I/Os to the interface instead

--- script_support/additional_configurations/SPI_LOOPBACK/SPI_LOOPBACK.tcl
+++ script_support/additional_configurations/SPI_LOOPBACK/SPI_LOOPBACK.tcl
@@ -4,10 +4,21 @@ delete_component -component_name {ICICLE_MSS}
 import_mss_component -file "$local_dir/script_support/components/MSS_SPI_LOOPBACK/ICICLE_MSS.cxz"
 sd_update_instance -sd_name ${sd_name} -instance_name {ICICLE_MSS} 
 
-sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"QSPI_DATA2_BIBUF"} 
-sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"QSPI_DATA3_OR_PWM_BIBUF"} 
-sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"QSPI_DATA3_OR_PWM_D_OR"} 
-sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"QSPI_DATA3_OR_PWM_EN_OR"} 
+sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"BIBUF_0"} 
+sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"BIBUF_1"} 
+sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"BIBUF_2"} 
+sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"BIBUF_3"} 
+sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"BIBUF_4"} 
+sd_delete_instances -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -instance_names {"BIBUF_5"} 
+
+sd_instantiate_macro -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -macro_name {BIBUF} -instance_name {SPI_0_CLK} 
+sd_connect_pins -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -pin_names {"SPI_0_CLK:E" "ICICLE_MSS:SPI_0_CLK_OE_M2F"} 
+sd_connect_pins -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -pin_names {"SPI_0_CLK:D" "ICICLE_MSS:SPI_0_CLK_M2F"} 
+sd_connect_pins -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -pin_names {"SPI_0_CLK:Y" "ICICLE_MSS:SPI_0_CLK_F2M"} 
+sd_rename_port -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -current_port_name {PAD} -new_port_name {SPI_0_CLK_PAD} 
+sd_connect_pin_to_port -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -pin_name {ICICLE_MSS:SPI_0_DO_M2F} -port_name {SPI_0_DO} 
+sd_connect_pin_to_port -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -pin_name {ICICLE_MSS:SPI_0_DI_F2M} -port_name {SPI_0_DI} 
+sd_connect_pin_to_port -sd_name {MPFS_ICICLE_KIT_BASE_DESIGN} -pin_name {ICICLE_MSS:SPI_0_SS_F2M} -port_name {SPI_0_SS} 
 
 generate_component -component_name {MPFS_ICICLE_KIT_BASE_DESIGN} -recursive 0 
 

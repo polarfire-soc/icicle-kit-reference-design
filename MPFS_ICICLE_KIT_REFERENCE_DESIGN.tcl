371998d6c9c25a10815ec54a24d5cfb700046fc9
M31864 <hugh.breslin@microchip.com>
2022-09-27 11:24:35 +0100

    HSS update: update file name
    Updated the file name of the hex file that will be used in the envm client for HSS_UPDATE to match the new naming convention used by the HSS

--- MPFS_ICICLE_KIT_REFERENCE_DESIGN.tcl
+++ MPFS_ICICLE_KIT_REFERENCE_DESIGN.tcl
@@ -320,7 +320,7 @@ if {[info exists HSS_UPDATE]} {
         if {[catch    {exec wget https://github.com/polarfire-soc/hart-software-services/releases/latest/download/hss-envm-wrapper.mpfs-icicle-kit-es.hex -P ./script_support/} issue]} {
         }
     }
-    create_eNVM_config "$local_dir/script_support/components/MSS/ENVM.cfg" "$local_dir/script_support/hss-bm1-p0.hex"
+    create_eNVM_config "$local_dir/script_support/components/MSS/ENVM.cfg" "$local_dir/script_support/hss-envm-wrapper.mpfs-icicle-kit-es.hex"
     run_tool -name {GENERATEPROGRAMMINGDATA}
     configure_envm -cfg_file {script_support/components/MSS/ENVM.cfg}
 }
